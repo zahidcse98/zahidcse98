cb14337ae9809b1b0911da870a574d0359adcd2d
Zahid Hasan <zahidcse98@gmail.com>
2021-10-15 21:08:21 +0600

    show the sequence to the user with animations and sounds

--- game.js
+++ game.js
@@ -5,4 +5,15 @@ function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    flushTheButton(randomChosenColor);
+    playSound(randomChosenColor);
+}
+
+function flushTheButton(color) {
+    $("#" + color).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function playSound(color) {
+    let audio = new Audio('./sounds/' + color + '.mp3');
+    audio.play();
 }
