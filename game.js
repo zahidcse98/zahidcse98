0662d2bb015abdf4f22b533a3162fe37596dfffd
Zahid Hasan <zahidcse98@gmail.com>
2021-10-15 20:53:29 +0600

    a sequence function added and using a random number we chosse a random color from given colors

---
+++ game.js
@@ -0,0 +1,12 @@
+let buttonColors = ['red', 'green', 'blue', 'yellow'];
+let gamePattern = [];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+}
+
+
+nextSequence();
+console.log(gamePattern);
